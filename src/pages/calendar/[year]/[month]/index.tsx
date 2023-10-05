a40e251eaf46f7c0344b3d83fa8d26f79e39986d
Joowon <tkdel222@gmail.com>
2023-10-05 09:39:46 +0900

    update container scss

--- src/pages/calendar/[year]/[month]/index.tsx
+++ src/pages/calendar/[year]/[month]/index.tsx
@@ -3,16 +3,37 @@ import MonthDetailGrid from "@/assets/Calendar/Grid/MonthDetailGrid";
 import FriendPage from "@/components/PartialPages/Friend/Friend";
 import MonthPage from "@/components/PartialPages/Month/Month";
 import SchedulePage from "@/components/PartialPages/Schedule/Schedule";
-
+import styles from "./index.module.scss";
+import useSWR from "swr";
+import axios from "axios";
+import { useRouter } from "next/router";
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+}
+interface Schedules {
+  user: User;
+  schedules: boolean[]; // ex) [true(00:00), ... false(23:45)]
+}
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 const MonthDetailPage = () => {
+  const router = useRouter();
+  const { data, error, mutate } = useSWR<Schedules>(
+    `/api/schedule?year=${router.query.year}&month=${router.query.month}`,
+    fetcher
+  );
   return (
-    <MonthDetailGrid>
-      <FriendDrawer>
-        <FriendPage />
-      </FriendDrawer>
-      <MonthPage />
-      <SchedulePage />
-    </MonthDetailGrid>
+    <div className={styles.container}>
+      <MonthDetailGrid>
+        <FriendDrawer>
+          <FriendPage />
+        </FriendDrawer>
+        <MonthPage />
+        <SchedulePage />
+      </MonthDetailGrid>
+    </div>
   );
 };
 
