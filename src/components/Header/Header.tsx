53f188d654d6e4e928ae4ab6a5f6e54274c58cd3
Joowon <tkdel222@gmail.com>
2023-10-08 20:22:04 +0900

    fix bug on today button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,17 @@
 import dayjs from "dayjs";
 import styles from "./Header.module.scss";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import BackIcon from "@public/back-white.png";
 import { signIn, signOut, useSession } from "next-auth/react";
-
+import axios from "axios";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import {
   getFriendDrawerSwitch,
   setSwitch,
 } from "@/redux/featrues/friendDrawerSwitchSlice";
+import Link from "next/link";
 const Header = () => {
   const now = dayjs();
   const router = useRouter();
@@ -31,13 +31,11 @@ const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles.grid}>
-        <div>
+        <div className={styles.left}>
           {Object.keys(router.query).includes("year") && (
-            <div
+            <Link
               className={styles.back}
-              onClick={() => {
-                router.push(`/calendar/years?year=${year}`);
-              }}>
+              href={`/years?year=${year}`}>
               <div className={styles.icon}>
                 <Image
                   src={BackIcon}
@@ -47,23 +45,34 @@ const Header = () => {
                 />
               </div>
               <div className={styles.text}>{`${year}년`}</div>
-            </div>
+            </Link>
           )}
         </div>
 
-        <div>
-          <div>
-            <Link href={`/calendar/years#year${now.year()}`}>오늘</Link>
-          </div>
-          <div
+        <div className={styles.center}>
+          {router.pathname.includes("years") && (
+            <div>
+              <a href={`/years#year${now.year()}`}>오늘</a>
+            </div>
+          )}
+          {Object.keys(router.query).includes("month") && (
+            <div
+              onClick={() => {
+                dispatch(setSwitch(!isFriendOn));
+              }}>
+              친구 목록
+            </div>
+          )}
+          {/* <div
             onClick={() => {
-              dispatch(setSwitch(!isFriendOn));
+              axios.get("/api/user/friend").then((res) => {
+                console.log(res.data);
+              });
             }}>
-            친구 목록
-          </div>
-          <div>{session?.user?.name}</div>
+            {session?.user?.name}
+          </div> */}
         </div>
-        <div>
+        <div className={styles.right}>
           {session?.user ? (
             <div
               onClick={() => {
@@ -84,6 +93,7 @@ const Header = () => {
               src={session?.user?.image as string}
               alt={""}
               fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
         </div>
