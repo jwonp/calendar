607acd1750ee74662f12d6f336e8931d3ac17735
Joowon <tkdel222@gmail.com>
2023-10-09 20:13:19 +0900

    update routing to /calendar/[year]/[month]

--- src/components/Pages/years/Years.tsx
+++ src/components/Pages/years/Years.tsx
@@ -1,17 +1,15 @@
-
 import dayjs from "dayjs";
 
 import { useEffect, useState } from "react";
-
 import styles from "./Years.module.scss";
 import Calendar from "@/assets/Calendar/Calendar";
 import { getAllFirstDays } from "@/utils/dateUtils";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CalendarYearPage = () => {
   const now = dayjs();
   const router = useRouter();
-  const searchParams = useSearchParams();
 
   const [days, setDays] = useState<dayjs.Dayjs[]>([]);
   useEffect(() => {
@@ -20,44 +18,59 @@ const CalendarYearPage = () => {
     }
     const allFirstDays = getAllFirstDays(now);
     setDays(allFirstDays);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   useEffect(() => {
-    if (searchParams.get("year")) {
-      router.push(`/calendar/years#year${searchParams.get("year")}`);
+    if (days.length === 0) {
+      return;
     }
-  }, [router, searchParams]);
+    if (router?.query?.year) {
+      router.push(`/years#year${router?.query?.year}`);
+    }
+  }, [router, days]);
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
-        {days.map((item, index) => {
-          if (item.month() === 0) {
+        {days.length > 1 &&
+          days.map((item, index) => {
+            if (item.month() === 0) {
+              return (
+                <>
+                  <div
+                    id={`year${item.year()}`}
+                    className={styles.indicator}></div>
+                  <div
+                    className={styles.gridOneLine}
+                    key={`year${item.year()}`}>
+                    <div>{`${item.year()} 년`}</div>
+                  </div>
+                  <Link
+                    className={styles.month}
+                    key={index}
+                    href={`/calendar/[year]/[month]`}
+                    as={`/calendar/${item.year()}/${item.month() + 1}`}>
+                    <Calendar
+                      year={item.year().toString()}
+                      month={(item.month() + 1).toString()}
+                    />
+                  </Link>
+                </>
+              );
+            }
+
             return (
-              <>
-                <div
-                  id={`year${item.year()}`}
-                  className={styles.indicator}></div>
-                <div
-                  className={styles.gridOneLine}
-                  key={`year${item.year()}`}>
-                  <div>{`${item.year()} 년`}</div>
-                </div>
+              <Link
+                className={styles.month}
+                key={index}
+                href={`/calendar/[year]/[month]`}
+                as={`/calendar/${item.year()}/${item.month() + 1}`}>
                 <Calendar
-                  key={index}
                   year={item.year().toString()}
                   month={(item.month() + 1).toString()}
                 />
-              </>
+              </Link>
             );
-          }
-
-          return (
-            <Calendar
-              key={index}
-              year={item.year().toString()}
-              month={(item.month() + 1).toString()}
-            />
-          );
-        })}
+          })}
       </div>
     </div>
   );
