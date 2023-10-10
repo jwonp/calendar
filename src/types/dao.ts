119c30e924db7e126cf766daf216ef3f9e7052be
Joowon <tkdel222@gmail.com>
2023-10-10 22:26:09 +0900

    add data types

---
+++ src/types/dao.ts
@@ -0,0 +1,32 @@
+export interface Date {
+  year: number; // YYYY
+  month: number; // 1~12
+  date: number; // 1~31
+  day: number; // 0(sun) ~ 6(sat)
+}
+
+export interface Schedule {
+  userDocId: string;
+  date: Date;
+  schedule: boolean[]; //[true(00:00), ... , false(23:45)]
+}
+
+export interface FriendRequest {
+  applicantDocId: string;
+  isRejected: boolean;
+  isAccepted: boolean;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  picture: string;
+  friends: string[];
+}
+
+export interface Group {
+  docId: string;
+  title: string;
+  members: string[];
+}
---
+++ src/types/dto.ts
@@ -0,0 +1,29 @@
+import { FriendRequest as FriendRequestDAO } from "./dao";
+export interface UserDetail {
+  id: string;
+  name: string;
+  email: string;
+  picture: string;
+  friends: string[];
+  docId: string;
+}
+export interface FriendRequest {
+  fromDocId: string;
+  toDocId: string;
+}
+export interface RequestReply extends FriendRequestDAO {
+  userDocId: string;
+}
+export interface Group {
+  host: string;
+  title: string;
+}
+
+export interface Schedule {
+  userDocId: string;
+  year: number; // YYYY
+  month: number; // 1~12
+  date: number; // 1~31
+  day: number; // 0(sun) ~ 6(sat)
+  schedule: boolean[];
+}
