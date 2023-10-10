77f95376ac630af2023ec902edce9011061634eb
Joowon <tkdel222@gmail.com>
2023-10-10 16:50:49 +0900

    update document id on session.user

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,12 @@
 import NextAuth, { DefaultSession } from "next-auth";
 
-
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session extends DefaultSession {
     user?: {
+      docId?: string | null;
       name?: string | null;
       email?: string | null;
       image?: string | null;
