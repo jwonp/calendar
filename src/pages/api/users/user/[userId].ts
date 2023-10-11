4831aee7e20dee3d4dec05e7e3f014b9ff96dbc9
Joowon <tkdel222@gmail.com>
2023-10-11 17:35:32 +0900

    add api for GET /users/user/{userId}

---
+++ src/pages/api/users/user/[userId].ts
@@ -0,0 +1,26 @@
+import { AxiosWithAuthorization, methods } from "@/utils/request";
+import { NextApiRequest, NextApiResponse } from "next";
+import { JWT, getToken } from "next-auth/jwt";
+import { Error } from "@/types/types";
+const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  const { userId } = req.query;
+
+  const result = await AxiosWithAuthorization(
+    req,
+    `/users/user/${userId}`,
+    methods.GET
+  );
+
+  if (Math.floor(result.status / 100) >= 4) {
+    const error: Error = {
+      message: "해당 요청 중 오류가 발생했습니다.",
+    };
+    res.status(result.status).json(error);
+  }
+  res.status(200).json({
+    path: `/users/user/${userId}`,
+    method: req.method,
+    data: result,
+  });
+};
+export default handler;
