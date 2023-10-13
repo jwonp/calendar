603a567f41685e4d7d8f4d260b49555701ef4b57
Joowon <tkdel222@gmail.com>
2023-10-13 21:58:38 +0900

    update search bar

--- src/components/PartialPages/Friend/Friend.tsx
+++ src/components/PartialPages/Friend/Friend.tsx
@@ -2,6 +2,12 @@ import UserBlock, {
   UserBlockProps,
 } from "@/assets/Calendar/Drawer/UserBlock/UserBlock";
 import styles from "./Friend.module.scss";
+import { use, useEffect } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { DefaultFetcher, UrlBuilder } from "@/utils/SwrConfig";
+import { useSession } from "next-auth/react";
+import SearchBar from "@/components/SearchBar/SearchBar";
 const mockUserBlockData: UserBlockProps[] = [
   {
     name: "q",
@@ -11,9 +17,20 @@ const mockUserBlockData: UserBlockProps[] = [
   },
 ];
 const FriendPage = () => {
+  const { data: session } = useSession();
+  const FriendSWR = useSWR(
+    UrlBuilder(`/api/users/friend/${session?.user?.id}`),
+    DefaultFetcher
+  );
+  useEffect(() => {
+    console.log(FriendSWR.data);
+  }, [FriendSWR.data]);
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        <SearchBar />
+      </div>
+      <div className={styles.grid}>
         <div>친구 요청</div>
         {mockUserBlockData.map((item, index) => (
           <UserBlock
@@ -22,7 +39,7 @@ const FriendPage = () => {
           />
         ))}
       </div>
-      <div> 
+      <div className={styles.grid}>
         <div>친구 목록</div>
         <div></div>
       </div>
