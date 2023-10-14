b79a160298df067aba82781dee7887e9c58dd3c5
Joowon <tkdel222@gmail.com>
2023-10-14 18:48:55 +0900

    update to handle friends and friends request

--- src/components/PartialPages/Friend/Friend.tsx
+++ src/components/PartialPages/Friend/Friend.tsx
@@ -2,12 +2,13 @@ import UserBlock, {
   UserBlockProps,
 } from "@/assets/Calendar/Drawer/UserBlock/UserBlock";
 import styles from "./Friend.module.scss";
-import { use, useEffect } from "react";
+import { use, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import { DefaultFetcher, UrlBuilder } from "@/utils/SwrConfig";
 import { useSession } from "next-auth/react";
 import SearchBar from "@/components/SearchBar/SearchBar";
+import { User } from "@/types/dao";
 const mockUserBlockData: UserBlockProps[] = [
   {
     name: "q",
@@ -18,30 +19,50 @@ const mockUserBlockData: UserBlockProps[] = [
 ];
 const FriendPage = () => {
   const { data: session } = useSession();
-  const FriendSWR = useSWR(
+
+  const FriendSWR = useSWR<Omit<User, "friends" | "docId">[]>(
     UrlBuilder(`/api/users/friend/${session?.user?.id}`),
     DefaultFetcher
   );
-  useEffect(() => {
-    console.log(FriendSWR.data);
-  }, [FriendSWR.data]);
+  const FriendRequestSWR = useSWR<Omit<User, "friends">[]>(
+    UrlBuilder(`/api/users/friend/request/${session?.user?.docId}`)
+  );
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
         <SearchBar />
       </div>
+
       <div className={styles.grid}>
         <div>친구 요청</div>
-        {mockUserBlockData.map((item, index) => (
-          <UserBlock
-            key={index}
-            {...item}
-          />
-        ))}
+        <div>
+          {FriendRequestSWR.data &&
+            FriendRequestSWR.data.map((friend, index) => (
+              <UserBlock
+                key={index}
+                userId={friend.id}
+                name={friend.name}
+                email={friend.email}
+                image={friend.picture}
+              />
+            ))}
+        </div>
       </div>
+
       <div className={styles.grid}>
         <div>친구 목록</div>
-        <div></div>
+        <div>
+          {FriendSWR.data &&
+            FriendSWR.data.map((friend, index) => (
+              <UserBlock
+                key={index}
+                userId={friend.id}
+                name={friend.name}
+                email={friend.email}
+                image={friend.picture}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
