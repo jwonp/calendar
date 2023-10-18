79c44ed62ed21726ed15d79c261cab34241a6126
Joowon <tkdel222@gmail.com>
2023-10-18 21:58:58 +0900

    update useMemo to render lists

--- src/components/PartialPages/Friend/Friend.tsx
+++ src/components/PartialPages/Friend/Friend.tsx
@@ -1,31 +1,33 @@
-import UserBlock, {
-  UserBlockProps,
-} from "@/assets/Calendar/Drawer/UserBlock/UserBlock";
+import UserBlock from "@/assets/Calendar/Drawer/UserBlock/UserBlock";
 import styles from "./Friend.module.scss";
-import { use, useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useMemo } from "react";
+
 import useSWR from "swr";
 import { DefaultFetcher, UrlBuilder } from "@/utils/SwrConfig";
 import { useSession } from "next-auth/react";
 import SearchBar from "@/components/SearchBar/SearchBar";
-import { User } from "@/types/dao";
+import { Group, User } from "@/types/dao";
 import { RequestReply } from "@/types/dto";
 import axios from "axios";
-const mockUserBlockData: UserBlockProps[] = [
-  {
-    name: "q",
-    email: "user234@google.com",
-    image: "",
-    withCheckButton: true,
-  },
-];
+import ListSelectButton from "@/assets/Calendar/Drawer/ListSelectButton/ListSelectButton";
+import { LIST, getListSelector } from "@/redux/featrues/ListSelectorSlice";
+import { useAppSelector } from "@/redux/hooks";
+import GroupBlock from "@/assets/Calendar/Drawer/GroupBlock/GroupBlock";
+
 const FriendPage = () => {
   const { data: session } = useSession();
-
+  const listSelector = useAppSelector(getListSelector);
   const FriendSWR = useSWR<Omit<User, "friends" | "docId">[]>(
     UrlBuilder(
       `/api/users/friend/${session?.user?.id}`,
       session?.user?.id !== undefined
+    ),
+    DefaultFetcher
+  );
+  const GroupSWR = useSWR<Group[]>(
+    UrlBuilder(
+      `/api/groups/group/${session?.user?.docId}`,
+      session?.user?.docId !== undefined
     ),
     DefaultFetcher
   );
@@ -36,10 +38,94 @@ const FriendPage = () => {
     ),
     DefaultFetcher
   );
-  useEffect(() => {
-    console.log(session?.user?.docId);
-    console.log(FriendRequestSWR.data);
+  const FriendRequestBlocks = useMemo(() => {
+    if (FriendRequestSWR.data === undefined) {
+      return (
+        <div className={styles.noContent}>친구 신청 목록이 비어있습니다</div>
+      );
+    }
+    try {
+      return FriendRequestSWR.data.map((friend, index) => (
+        <UserBlock
+          key={index}
+          userId={friend.id}
+          name={friend.name}
+          email={friend.email}
+          image={friend.picture}
+          onSubmit={() => {
+            if (!session?.user?.docId || !friend.docId) {
+              return;
+            }
+            const requestReply: RequestReply = {
+              userDocId: session?.user?.docId,
+              applicantDocId: friend.docId,
+              isRejected: false,
+              isAccepted: true,
+            };
+            axios.patch("/api/users/friend/request", requestReply);
+          }}
+          onCancel={() => {
+            if (!session?.user?.docId || !friend.docId) {
+              return;
+            }
+            const requestReply: RequestReply = {
+              userDocId: session?.user?.docId,
+              applicantDocId: friend.docId,
+              isRejected: true,
+              isAccepted: false,
+            };
+            axios.patch("/api/users/friend/request", requestReply);
+          }}
+        />
+      ));
+    } catch {
+      return (
+        <div className={styles.noContent}>친구 신청 목록이 비어있습니다</div>
+      );
+    }
   }, [FriendRequestSWR]);
+  const GroupList = useMemo(() => {
+    if (GroupSWR.data === undefined) {
+      return <div className={styles.noContent}>그룹 목록이 비어있습니다</div>;
+    }
+    try {
+      return GroupSWR.data.map((group, index) => (
+        <GroupBlock
+          key={index}
+          docId={group.docId}
+          title={group.title}
+          members={group.members}
+        />
+      ));
+    } catch {
+      return <div className={styles.noContent}>그룹 목록이 비어있습니다</div>;
+    }
+  }, [GroupSWR]);
+  const FriendList = useMemo(() => {
+    if (FriendSWR.data === undefined) {
+      return <div className={styles.noContent}>친구 목록이 비어있습니다</div>;
+    }
+    try {
+      return FriendSWR.data.map((friend, index) => (
+        <UserBlock
+          key={index}
+          userId={friend.id}
+          name={friend.name}
+          email={friend.email}
+          image={friend.picture}
+        />
+      ));
+    } catch {
+      return <div className={styles.noContent}>친구 목록이 비어있습니다</div>;
+    }
+  }, [FriendSWR]);
+  const ListBlocks = useMemo(() => {
+    if (listSelector === LIST.FRIEND) {
+      return FriendList;
+    }
+    return GroupList;
+  }, [listSelector, FriendList, GroupList]);
+
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
@@ -48,58 +134,21 @@ const FriendPage = () => {
 
       <div className={styles.grid}>
         <div>친구 요청</div>
-        <div>
-          {FriendRequestSWR.data &&
-            FriendRequestSWR.data.map((friend, index) => (
-              <UserBlock
-                key={index}
-                userId={friend.id}
-                name={friend.name}
-                email={friend.email}
-                image={friend.picture}
-                onSubmit={() => {
-                  if (!session?.user?.docId || !friend.docId) {
-                    return;
-                  }
-                  const requestReply: RequestReply = {
-                    userDocId: session?.user?.docId,
-                    applicantDocId: friend.docId,
-                    isRejected: false,
-                    isAccepted: true,
-                  };
-                  axios.patch("/api/users/friend/request", requestReply);
-                }}
-                onCancel={() => {
-                  if (!session?.user?.docId || !friend.docId) {
-                    return;
-                  }
-                  const requestReply: RequestReply = {
-                    userDocId: session?.user?.docId,
-                    applicantDocId: friend.docId,
-                    isRejected: true,
-                    isAccepted: false,
-                  };
-                  axios.patch("/api/users/friend/request", requestReply);
-                }}
-              />
-            ))}
-        </div>
+        <div>{FriendRequestBlocks}</div>
       </div>
 
       <div className={styles.grid}>
-        <div>친구 목록</div>
-        <div>
-          {FriendSWR.data &&
-            FriendSWR.data.map((friend, index) => (
-              <UserBlock
-                key={index}
-                userId={friend.id}
-                name={friend.name}
-                email={friend.email}
-                image={friend.picture}
-              />
-            ))}
+        <div className={styles.buttons}>
+          <ListSelectButton
+            text={"친구 목록"}
+            selector={LIST.FRIEND}
+          />
+          <ListSelectButton
+            text={"그룹 목록"}
+            selector={LIST.GROUP}
+          />
         </div>
+        <div>{ListBlocks}</div>
       </div>
     </div>
   );
