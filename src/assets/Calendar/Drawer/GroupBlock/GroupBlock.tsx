4f4b49d0d62722b324824fb5f49af60828b6951e
Joowon <tkdel222@gmail.com>
2023-10-22 21:07:34 +0900

    update group block to handle select group state

--- src/assets/Calendar/Drawer/GroupBlock/GroupBlock.tsx
+++ src/assets/Calendar/Drawer/GroupBlock/GroupBlock.tsx
@@ -1,21 +1,35 @@
-import { Group } from "@/types/dao";
+import { Group, User } from "@/types/dao";
 import styles from "./GroupBlock.module.scss";
+import { useAppDispatch } from "@/redux/hooks";
+import { setSelectedGroup} from "@/redux/featrues/groupSelectSlice";
 interface GroupBlockProps extends Group {}
-const GroupBlock = ({ title, members, docId }: GroupBlockProps) => {
-  const memmberList = (members: string[]) => {
+const GroupBlock = (group: GroupBlockProps) => {
+  const dispatch = useAppDispatch();
+  const memmberList = (members: Omit<User, "friends">[]) => {
     let displayText = "";
-    displayText = JSON.stringify(members);
-    displayText
+    const memberNames = members.map((member) => member.name);
+    const displayMemberNames = memberNames
+      .filter((_, index) => index < 3)
+      .filter((_, index) => index < 3);
+    displayText = JSON.stringify(displayMemberNames);
+    displayText = displayText
       .replaceAll(",", ", ")
       .replaceAll('"', " ")
       .replace("[", "")
       .replace("]", "");
+    if (memberNames.length > 3) {
+      return `${displayText} 외 ${memberNames.length - 3}명`;
+    }
     return displayText;
   };
   return (
-    <div>
-      <div>{title}</div>
-      <div>{memmberList(members)}</div>
+    <div
+      className={styles.container}
+      onClick={() => {
+        dispatch(setSelectedGroup(group));
+      }}>
+      <div>{group.title}</div>
+      <div>{memmberList(group.members)}</div>
     </div>
   );
 };
