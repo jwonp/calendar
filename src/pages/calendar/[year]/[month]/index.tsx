9ec98eff4b745b2ccc1cc876c0891e5aa6da67ae
Joowon <tkdel222@gmail.com>
2023-10-23 18:04:49 +0900

    update condition to display  friend, group  drawer

--- src/pages/calendar/[year]/[month]/index.tsx
+++ src/pages/calendar/[year]/[month]/index.tsx
@@ -1,15 +1,26 @@
-import FriendDrawer from "@/assets/Calendar/Drawer/FriendDrawer/FriendDrawer";
+import Drawer from "@/assets/Calendar/Drawer/Drawer/Drawer";
 import MonthDetailGrid from "@/assets/Calendar/Grid/MonthDetailGrid";
 import FriendPage from "@/components/PartialPages/Friend/Friend";
 import MonthPage from "@/components/PartialPages/Month/Month";
 import SchedulePage from "@/components/PartialPages/Schedule/Schedule";
 import styles from "./index.module.scss";
-import { useEffect } from "react";
-import { signIn, useSession } from "next-auth/react";
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+
+import GroupPage from "@/components/PartialPages/Group/Group";
+import { useAppSelector } from "@/redux/hooks";
+import { getDrawerPage } from "@/redux/featrues/drawerSwitchSlice";
 
 const MonthDetailPage = () => {
   const router = useRouter();
+  const drawerPage =useAppSelector(getDrawerPage)
   const { data: session } = useSession();
   useEffect(() => {
     if (!router.isReady) {
@@ -23,14 +34,15 @@ const MonthDetailPage = () => {
   return (
     <div className={styles.container}>
       <MonthDetailGrid>
-        <FriendDrawer>
-          <FriendPage />
-        </FriendDrawer>
+        <Drawer>
+          {drawerPage === "friend" ? <FriendPage /> : <GroupPage />}
+        </Drawer>
+
         <MonthPage />
         <SchedulePage />
       </MonthDetailGrid>
     </div>
   );
 };
-
+3;
 export default MonthDetailPage;
