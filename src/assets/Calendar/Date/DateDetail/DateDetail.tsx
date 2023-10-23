e98db9efc7ef2fecf606443130c8047d47300a5e
Joowon <tkdel222@gmail.com>
2023-10-23 18:03:48 +0900

    update onClick event

--- src/assets/Calendar/Date/DateDetail/DateDetail.tsx
+++ src/assets/Calendar/Date/DateDetail/DateDetail.tsx
@@ -1,19 +1,31 @@
 import styles from "./DateDetail.module.scss";
-import ScheduleLine from "../ScheduleLine/ScheduleLine";
+import ScheduleLine from "../Schedule/ScheduleLine/ScheduleLine";
+import { Schedule } from "@/types/dao";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { getNowDateWithDayjs, setDate } from "@/redux/featrues/nowDateSlice";
 interface DateDetail {
   date: number;
-  schedule?: object[];
+  schedule: Schedule;
 }
 const DateDetail = ({ date, schedule }: DateDetail) => {
+  const dispatch = useAppDispatch();
+  const nowDate = useAppSelector(getNowDateWithDayjs);
+
   return (
-    <div className={styles.container}>
-      <div>{date}</div>
+    <div
+      className={styles.container}
+      onClick={() => {
+        dispatch(setDate(date));
+      }}>
+      <div className={nowDate.date() === date ? styles.date : undefined}>
+        {date}
+      </div>
       <div className={styles.scheduleBox}>
         <div className={styles.schedules}>
           <ScheduleLine text={"일정 1"} />
           <ScheduleLine text={"일정 2"} />
         </div>
-          <div className={styles.etc}>외 2 명</div>
+        <div className={styles.etc}>외 2 명</div>
       </div>
     </div>
   );
