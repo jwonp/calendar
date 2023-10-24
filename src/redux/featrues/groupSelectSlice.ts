f37f1a91c848ebf38b5ad41c10bea2a993f06f89
Joowon <tkdel222@gmail.com>
2023-10-24 21:22:59 +0900

    update selectedGroup property

--- src/redux/featrues/groupSelectSlice.ts
+++ src/redux/featrues/groupSelectSlice.ts
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-import { User } from "@/types/dao";
+import { Group, User } from "@/types/dao";
+
 
 export interface GroupSelectState {
   friends: Omit<User, "friends">[];
   selected: Omit<User, "friends">[];
+  selectedGroup: Group;
 }
-
+const initGroup:Group = {
+  docId: "",
+  title: "",
+  members: []
+}
 const initialState: GroupSelectState = {
   friends: [],
   selected: [],
+  selectedGroup: initGroup,
 };
 
 export const groupSelectSlice = createSlice({
@@ -31,6 +38,12 @@ export const groupSelectSlice = createSlice({
         ...state.selected.filter((select) => select.id !== action.payload.id),
       ];
     },
+    setSelectedGroup: (state, action: PayloadAction<Group>) => {
+      state.selectedGroup = action.payload;
+    },
+    resetSelectedGroup: (state) => {
+      state.selectedGroup = initGroup;
+    },
     resetFriends: (state) => {
       state.friends = [];
     },
@@ -45,12 +58,16 @@ export const {
   setFriends,
   addSelected,
   removeSelected,
+  setSelectedGroup,
+  resetSelectedGroup,
   resetFriends,
   resetSelected,
 } = groupSelectSlice.actions;
 
 export const getGroupSelect = (state: RootState) => state.groupSelect;
 export const getGroupFriends = (state: RootState) => state.groupSelect.friends;
+export const getSelectedGroup= (state: RootState) =>
+  state.groupSelect.selectedGroup;
 export const getGroupSelected = (state: RootState) =>
   state.groupSelect.selected;
 
