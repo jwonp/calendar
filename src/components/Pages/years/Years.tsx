25880e419be3f209640337eca492b37ea2ff2a8a
Joowon <tkdel222@gmail.com>
2023-10-25 19:41:39 +0900

    refactor render years

--- src/components/Pages/years/Years.tsx
+++ src/components/Pages/years/Years.tsx
@@ -1,77 +1,52 @@
 import dayjs from "dayjs";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import styles from "./Years.module.scss";
-import Calendar from "@/assets/Calendar/Calendar";
-import { getAllFirstDays } from "@/utils/dateUtils";
-import { useRouter } from "next/router";
-import Link from "next/link";
+
+import {
+  add10YearsFromFirstYear,
+  add10YearsFromLastYear,
+  get10YearsFromThisYear,
+} from "@/utils/dateUtils";
+
+import Year from "@/assets/Year/Year";
 
 const CalendarYearPage = () => {
-  const now = dayjs();
-  const router = useRouter();
-
-  const [days, setDays] = useState<dayjs.Dayjs[]>([]);
+  const [years, setYears] = useState<number[]>([]);
   useEffect(() => {
-    if (!now) {
-      return;
-    }
-    const allFirstDays = getAllFirstDays(now);
-    setDays(allFirstDays);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    setYears(get10YearsFromThisYear(dayjs().year()));
   }, []);
-  useEffect(() => {
-    if (days.length === 0) {
-      return;
-    }
-    if (router?.query?.year) {
-      router.push(`/years#year${router?.query?.year}`);
-    }
-  }, [router, days]);
+  const Years = useMemo(() => {
+    return years.map((year, index) => (
+      <Year
+        key={index}
+        year={year}
+      />
+    ));
+  }, [years]);
   return (
-    <div className={styles.container}>
-      <div className={styles.grid}>
-        {days.length > 1 &&
-          days.map((item, index) => {
-            if (item.month() === 0) {
-              return (
-                <>
-                  <div
-                    id={`year${item.year()}`}
-                    className={styles.indicator}></div>
-                  <div
-                    className={styles.gridOneLine}
-                    key={`year${item.year()}`}>
-                    <div>{`${item.year()} 년`}</div>
-                  </div>
-                  <Link
-                    className={styles.month}
-                    key={index}
-                    href={`/calendar/[year]/[month]`}
-                    as={`/calendar/${item.year()}/${item.month() + 1}`}>
-                    <Calendar
-                      year={item.year().toString()}
-                      month={(item.month() + 1).toString()}
-                    />
-                  </Link>
-                </>
-              );
-            }
+    <div
+      className={styles.container}
+      onScroll={(e) => {
+        const isTop = e.currentTarget.scrollTop === 0;
+        const isBottom =
+          e.currentTarget.scrollHeight -
+            e.currentTarget.scrollTop -
+            e.currentTarget.offsetHeight <=
+          0.5;
+        const elHeight = Math.floor(
+          e.currentTarget.scrollHeight / years.length
+        );
 
-            return (
-              <Link
-                className={styles.month}
-                key={index}
-                href={`/calendar/[year]/[month]`}
-                as={`/calendar/${item.year()}/${item.month() + 1}`}>
-                <Calendar
-                  year={item.year().toString()}
-                  month={(item.month() + 1).toString()}
-                />
-              </Link>
-            );
-          })}
-      </div>
+        if (isTop) {
+          setYears(add10YearsFromFirstYear(years));
+          e.currentTarget.scroll({ top: elHeight * 10 });
+        }
+        if (isBottom) {
+          setYears(add10YearsFromLastYear(years));
+        }
+      }}>
+      {Years}
     </div>
   );
 };
