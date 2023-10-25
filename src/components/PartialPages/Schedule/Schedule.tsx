5b0a3108b86226cd999f20bf8545f3aec1837137
Joowon <tkdel222@gmail.com>
2023-10-25 21:00:53 +0900

    update filter user in group schedules

--- src/components/PartialPages/Schedule/Schedule.tsx
+++ src/components/PartialPages/Schedule/Schedule.tsx
@@ -5,11 +5,14 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { useAppSelector } from "@/redux/hooks";
 import { getNowDateWithDayjs } from "@/redux/featrues/nowDateSlice";
 import { Schedule as RowSchedule } from "@/types/dto";
-import { Schedule } from "@/types/dao";
+import { Group, Schedule, User } from "@/types/dao";
 import { useSession } from "next-auth/react";
 import axios from "axios";
 import useSWR from "swr";
 import { DefaultFetcher, UrlBuilder } from "@/utils/SwrConfig";
+import { getSelectedGroup } from "@/redux/featrues/groupSelectSlice";
+import { repeatGrid } from "@/utils/stringUtils";
+import MemberSchedule from "@/assets/Calendar/Date/Schedule/MemberSchedule/MemberSchedule";
 
 const hours = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -30,6 +33,8 @@ const createTimes = () => {
 
 const SchedulePage = () => {
   const router = useRouter();
+
+  const selectedGroup = useAppSelector(getSelectedGroup);
   const nowDate = useAppSelector(getNowDateWithDayjs);
   const { data: session } = useSession();
   const [times, setTimes] = useState<string[]>(createTimes());
@@ -37,7 +42,6 @@ const SchedulePage = () => {
     createTimes().map(() => false)
   );
   const url = useMemo(() => {
-    console.log("url");
     return `/api/schedules/user/${session?.user?.docId}/${
       router.query.year as string
     }/${router.query.month as string}`;
@@ -46,9 +50,79 @@ const SchedulePage = () => {
     UrlBuilder(url, session?.user?.docId !== undefined),
     DefaultFetcher
   );
+
+  const GroupScheduleSWR = useSWR<Schedule[]>(
+    UrlBuilder(
+      `/api/schedules/group/${selectedGroup.docId}/${
+        router.query.year as string
+      }/${router.query.month as string}`,
+      selectedGroup.docId !== "" && selectedGroup.docId !== undefined
+    ),
+    DefaultFetcher
+  );
   const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
   const selectFlag = useRef<boolean>(false);
   const loadingFlag = useRef<boolean>(false);
+  const GroupMemberScheduleMap = useMemo<
+    Map<number, Omit<User, "friends">[]>
+  >(() => {
+    if (!selectedGroup || !GroupScheduleSWR.data) {
+      return new Map<number, Omit<User, "friends">[]>();
+    }
+    //init memberMap
+    const memberMap = new Map<string, Omit<User, "friends">>();
+    selectedGroup.members.forEach((member) => {
+      memberMap.set(member.docId, member);
+    });
+    console.log({
+      title: "memberMap",
+      members: memberMap.entries(), //Array.from(memberMap.values()),
+    });
+    console.log({
+      title: "rowScheuldes",
+      rowSchedules: GroupScheduleSWR.data
+        .filter(
+          (schedule) =>
+            schedule.date.year === nowDate.year() &&
+            schedule.date.month === nowDate.month() + 1 &&
+            schedule.date.date === nowDate.date()
+        )
+       
+        .map((schedule) => {
+          return { userDocId: schedule.userDocId, schedule: schedule.schedule };
+        }),
+    });
+    const nowDataSchedule = GroupScheduleSWR.data.filter(
+      (schedule) =>
+        schedule.date.year === nowDate.year() &&
+        schedule.date.month === nowDate.month() + 1 &&
+        schedule.date.date === nowDate.date()
+    ) .filter((schedule)=> schedule.userDocId !== session?.user?.docId);
+    //init scheduleMap
+    const scheduleMap = new Map<number, Omit<User, "friends">[]>();
+    nowDataSchedule.forEach((schedule) => {
+      schedule.schedule.forEach((time, timeIndex) => {
+        if (time) {
+          const targetMember = memberMap.get(schedule.userDocId);
+          const preMembers = scheduleMap.get(timeIndex);
+          if (targetMember !== undefined) {
+            if (preMembers) {
+              scheduleMap.set(timeIndex, [...preMembers, targetMember]);
+            } else {
+              scheduleMap.set(timeIndex, [targetMember]);
+            }
+          }
+        }
+      });
+    });
+    console.log({
+      title: "scheduleMap",
+      schedules: Array.from(scheduleMap.values()),
+    });
+
+    return scheduleMap;
+  }, [selectedGroup, GroupScheduleSWR]);
+
   useEffect(() => {
     const isNotReadyToLoading =
       !ScheduleSWR.data || loadingFlag.current === false;
@@ -57,10 +131,10 @@ const SchedulePage = () => {
     }
 
     if (ScheduleSWR.data) {
-      ScheduleSWR.mutate();
       const nowScheduleArray = ScheduleSWR.data.filter(
         (schedule) => schedule.date.date === nowDate.date()
       );
+
       if (nowScheduleArray.length > 1 || nowScheduleArray.length === 0) {
         setSelected(createTimes().map(() => false));
         return;
@@ -72,13 +146,14 @@ const SchedulePage = () => {
     }
   }, [ScheduleSWR.data, nowDate]);
   useEffect(() => {
-    console.log(nowDate);
+    ScheduleSWR.mutate();
     loadingFlag.current = true;
   }, [nowDate]);
+
   return (
     <div className={styles.container}>
       <div className={styles.scrollBox}>
-        <div className={styles.flexBox}>
+        <div className={styles.gridBox}>
           <div>
             {times.map((time, index) => (
               <div
@@ -143,7 +218,31 @@ const SchedulePage = () => {
                   }
                   selectedItems[timeIndex] = selectFlag.current;
                   setSelected(() => [...selectedItems]);
-                }}></div>
+                }}>
+                <div></div>
+                <div
+                  className={styles["test"]}
+                  style={{
+                    gridTemplateColumns: GroupMemberScheduleMap.get(timeIndex)
+                      ? repeatGrid(
+                          "1fr",
+                          (
+                            GroupMemberScheduleMap.get(timeIndex) as Omit<
+                              User,
+                              "friends"
+                            >[]
+                          ).length
+                        )
+                      : "1fr",
+                  }}>
+                  {GroupMemberScheduleMap.get(timeIndex)?.map((member, index) => (
+                      <MemberSchedule
+                        key={`${timeIndex}-${index}`}
+                        {...member}
+                      />
+                    ))}
+                </div>
+              </div>
             ))}
           </div>
         </div>
