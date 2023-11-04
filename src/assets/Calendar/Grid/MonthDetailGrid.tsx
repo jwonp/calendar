d57aa6fe5a8aa5c4e65339e2a1aac8c535c3ed7d
Joowon <tkdel222@gmail.com>
2023-11-04 09:45:23 +0900

    update drawer switch

--- src/assets/Calendar/Grid/MonthDetailGrid.tsx
+++ src/assets/Calendar/Grid/MonthDetailGrid.tsx
@@ -1,14 +1,10 @@
 import { useAppSelector } from "@/redux/hooks";
 import styles from "./MonthDetailGrid.module.scss";
-import Provider from "@/redux/Provider";
-import { getFriendDrawerSwitch } from "@/redux/featrues/friendDrawerSwitchSlice";
 
-const MonthDetailGrid = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const friend = useAppSelector(getFriendDrawerSwitch);
+import { getDrawerSwitch } from "@/redux/featrues/drawerSwitchSlice";
+
+const MonthDetailGrid = ({ children }: { children: React.ReactNode }) => {
+  const friend = useAppSelector(getDrawerSwitch);
   return (
     <div className={`${styles.conatainer} ${friend ? styles.isFriend : ""}`}>
       {children}
