fc85f8d074eaab39757d8bdda517d14b3ece2eda
Joowon <tkdel222@gmail.com>
2024-03-03 22:03:29 +0900

    update timeslot design

--- src/components/PartialPages/Schedule/TimeSlot/TimeSlot.tsx
+++ src/components/PartialPages/Schedule/TimeSlot/TimeSlot.tsx
@@ -26,17 +26,33 @@ const TimeSlot = ({
       <div className={styles.time}></div>
       <div
         className={styles.members}
-        style={{
-          gridTemplateColumns: groupMemberSchedule
-            ? repeatGrid("1fr", groupMemberSchedule.length)
-            : "1fr",
-        }}>
-        {groupMemberSchedule?.map((member, index) => (
-          <MemberSchedule
-            key={`${timeIndex}-${index}`}
-            {...member}
-          />
-        ))}
+        // style={{
+        //   gridTemplateColumns: groupMemberSchedule
+        //     ? repeatGrid("1fr", groupMemberSchedule.length)
+        //     : "1fr",
+        // }}
+      >
+        {groupMemberSchedule
+          ?.filter((_, index) => index < 4)
+          .map((member, index) => {
+            if (index === 3) {
+              return (
+                <MemberSchedule
+                  key={`${timeIndex}-${index}`}
+                  docId={""}
+                  id={""}
+                  name={`${member.name} 외 ${groupMemberSchedule.length - 4}명`}
+                  email={""}
+                  picture={""}></MemberSchedule>
+              );
+            }
+            return (
+              <MemberSchedule
+                key={`${timeIndex}-${index}`}
+                {...member}
+              />
+            );
+          })}
       </div>
     </div>
   );
